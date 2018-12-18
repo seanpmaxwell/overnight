683f601e07ecab49b79679e695c0be5c680d4805
seanpmaxwell <seanpmaxwell1@gmail.com>
2018-12-17 21:42:26 -0800

    added middleware decorator

--- src/jwt/src/index.ts
+++ src/jwt/src/index.ts
@@ -5,50 +5,73 @@
  */
 
 
-const JWT = process.env.OVERNIGHTJWTSECRET
+// Pull in environment variables
+const SECRET: string | undefined = process.env.OVERNIGHTJWTSECRET
 const EXP = process.env.OVERNIGHTJWTEXP
 
 
-/**
- *
- */
-function setupMiddlware(): any
+
+
+/***********************************************************************************************
+ *                                          Middleware
+ **********************************************************************************************/
+
+function setupMiddlware(secret: string): RequestHandler
 {
     let opts = {
-        secret: 'someRandomString',
+        secret: secret,
         userProperty: 'payload'
     }
 
     return expressJwt(opts) // pick up here
 }
 
-export var middleware: string = setupMiddlware()
+export let middleware: string = setupMiddlware(SECRET || '')
 
 
 
-/**
- *
- */
-export function getJwt(info: {})
+
+/***********************************************************************************************
+ *                                          JWT
+ **********************************************************************************************/
+
+function setupJwt(dataToEcrypt: any, secret: string, expirationTime: string)
 {
+    let exp = {
+        expiresIn: expirationTime
+    }
 
+    return jsonwebtoken.sign(dataToEcrypt, secret, exp)
+}
+
+export function jwt(dataToEcrypt: any)
+{
+    return setupJwt(dataToEcrypt, SECRET || '', EXP || '3 days')
 }
 
 
 
-/**
- *
- */
+
+/***********************************************************************************************
+ *                                    Class for manual setup
+ **********************************************************************************************/
+
 export class jwtHandler
 {
-    getJwt(): void
+    getJwt(dataToEcrypt: any, secret: string, expirationTime: string): void
     {
-
+        setupJwt(dataToEcrypt, secret, expirationTime)
     }
 
-    getMiddleware(): void
+    getMiddleware(secret: string): void
     {
-
+        return setupMiddlware(secret)
     }
 }
+
+
+// For routes pass with Jwt Middleware
+// export interface SecureRequest extends Request {
+//     payload: any
+// }
 

bb9445567e7acce67d894ab89cdec7b9782f6803
Anand Chowdhary <mail@anandchowdhary.com>
2019-05-26 13:05:30 +0200

    Add support for *all* express verbs

--- src/core/lib/decorators.ts
+++ src/core/lib/decorators.ts
@@ -14,28 +14,96 @@ type Middlware = (req: Request, res: Response, next: NextFunction) => any;
  *                                      Method Decorators
  **********************************************************************************************/
 
+export function Checkout(path?: string): MethodDecorator {
+    return helperForRoutes('checkout', path);
+}
+
+export function Copy(path?: string): MethodDecorator {
+    return helperForRoutes('copy', path);
+}
+
+export function Delete(path?: string): MethodDecorator {
+    return helperForRoutes('delete', path);
+}
+
 export function Get(path?: string): MethodDecorator {
     return helperForRoutes('get', path);
 }
 
-
-export function Post(path?: string): MethodDecorator {
-    return helperForRoutes('post', path);
+export function Head(path?: string): MethodDecorator {
+    return helperForRoutes('head', path);
 }
 
-
-export function Put(path?: string): MethodDecorator {
-    return helperForRoutes('put', path);
+export function Lock(path?: string): MethodDecorator {
+    return helperForRoutes('lock', path);
 }
 
+export function Merge(path?: string): MethodDecorator {
+    return helperForRoutes('merge', path);
+}
+
+export function Mkactivity(path?: string): MethodDecorator {
+    return helperForRoutes('mkactivity', path);
+}
+
+export function Mkcol(path?: string): MethodDecorator {
+    return helperForRoutes('mkcol', path);
+}
+
+export function Move(path?: string): MethodDecorator {
+    return helperForRoutes('move', path);
+}
+
+export function MSearch(path?: string): MethodDecorator {
+    return helperForRoutes('m-search', path);
+}
+
+export function Notify(path?: string): MethodDecorator {
+    return helperForRoutes('notify', path);
+}
+
+export function Options(path?: string): MethodDecorator {
+    return helperForRoutes('options', path);
+}
 
 export function Patch(path?: string): MethodDecorator {
     return helperForRoutes('patch', path);
 }
 
+export function Post(path?: string): MethodDecorator {
+    return helperForRoutes('post', path);
+}
 
-export function Delete(path?: string): MethodDecorator {
-    return helperForRoutes('delete', path);
+export function Purge(path?: string): MethodDecorator {
+    return helperForRoutes('purge', path);
+}
+
+export function Put(path?: string): MethodDecorator {
+    return helperForRoutes('put', path);
+}
+
+export function Report(path?: string): MethodDecorator {
+    return helperForRoutes('report', path);
+}
+
+export function Search(path?: string): MethodDecorator {
+    return helperForRoutes('search', path);
+}
+
+export function Subscribe(path?: string): MethodDecorator {
+    return helperForRoutes('subscribe', path);
+}
+
+export function Trace(path?: string): MethodDecorator {
+    return helperForRoutes('trace', path);
+}
+
+export function Unlock(path?: string): MethodDecorator {
+    return helperForRoutes('unlock', path);
+}
+
+export function Unsubscribe(path?: string): MethodDecorator {
+    return helperForRoutes('unsubscribe', path);
 }
 
 
